df9771343f8f08d93d762cbcc5a6ad475fcb5e50
inepicone <inespicone@hotmail.com>
2025-05-09 10:28:18 +0300

    obtengo mas datos y los guardo en el json

--- scraper.py
+++ scraper.py
@@ -19,9 +19,29 @@ def format_events(raw_events):
     formatted = []
     for event in raw_events:
         title = event.get("name", {}).get("en") or event.get("name", {}).get("fi") or "Untitled"
+        description = event.get("description", {}).get("en") or event.get("description", {}).get("fi", "")
         start_time = event.get("start_time", "N/A")
         end_time = event.get("end_time", "N/A")
-        info_url_dict = event.get("info_url", {})
+
+        location = event.get("location", {}).get("name", {}).get("en") or ""
+        address_obj = event.get("location", {}).get("address", {}).get("street_address", {})
+        address = address_obj.get("en") or address_obj.get("fi") or ""
+        latitude = event.get("location", {}).get("position", {}).get("lat")
+        longitude = event.get("location", {}).get("position", {}).get("lon")
+
+        offers = event.get("offers", [])
+        is_free = offers[0].get("is_free", False) if offers else False
+
+        min_age = event.get("audience_min_age")
+        max_age = event.get("audience_max_age")
+
+        event_status = event.get("event_status", "")
+
+        images = [img.get("url") for img in event.get("images", []) if img.get("url")]
+        keywords = [kw.get("name", {}).get("en") or kw.get("name", {}).get("fi") for kw in event.get("keywords", [])]
+        provider = event.get("provider", "")
+
+        info_url_dict = event.get("info_url") or {}
         info_url = (
             info_url_dict.get("en") or
             info_url_dict.get("fi") or
@@ -29,7 +49,6 @@ def format_events(raw_events):
             ""
         )
         event_id = event.get("id", "")
-
         if info_url and info_url.startswith("http"):
             link = info_url
         elif event_id:
@@ -39,8 +58,20 @@ def format_events(raw_events):
 
         formatted.append({
             "title": title,
+            "description": description,
             "start_time": start_time,
             "end_time": end_time,
+            "location": location,
+            "address": address,
+            "latitude": latitude,
+            "longitude": longitude,
+            "is_free": is_free,
+            "audience_min_age": min_age,
+            "audience_max_age": max_age,
+            "event_status": event_status,
+            "images": images,
+            "keywords": keywords,
+            "provider": provider,
             "link": link
         })
     return formatted
